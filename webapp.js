a76380d845fe712cb9379c0474c179aae0b69d83
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-10-26 12:08:00 -0400

    Changes id to _id now that mongo is integrated. Reworks webapp file to have the server functionality based around MongoDB

--- webapp.js
+++ webapp.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
+var db;
 
 app.use(express.static('static'));
 
@@ -12,8 +14,11 @@ var bugData = [
 ];
 
 app.get('/api/bugs', function(req, res) {
-	res.json(bugData);
+	db.collection("bugs").find().toArray(function(err, doc) {
+		res.json(docs);
+	});
 });
+
 app.use(bodyParser.json());
 app.post('/api/bugs/', function(req, res) {
 	console.log(req.body);
@@ -23,7 +28,10 @@ app.post('/api/bugs/', function(req, res) {
 	res.json(req.body);
 });
 
-var server = app.listen(3000, function() {
-	var port = server.address().port;
-	console.log('Started server at port', port);
+MongoClient.connect('mongodb://localhost/bugsdb', function(err, dbConnection) {
+	db = dbConnection;
+	var server = app.listen(3000, function() {
+		var port = server.address().port;
+		console.log("Started server at port", port);
+	});
 });
