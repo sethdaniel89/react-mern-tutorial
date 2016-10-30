1ce38fa9264c72434e57e34d6b3c9f95c6177e73
Seth Goldman <seth.daniel.goldman@gmail.com>
2016-10-30 10:54:50 -0400

    Writes the gulp file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,11 @@
 var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 gulp.task('default', function() {
-	// place code
-})
+	return browserify('src/App.js')
+		.transform('babelify, {presets: react'})
+		.bundle()
+		.pipe(source('bundle.js'))
+		.pipe(gulp.('static/'));
+});
